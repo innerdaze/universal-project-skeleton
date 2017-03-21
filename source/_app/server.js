605b4561b74c0e4592a07eb0eb0c9a38a567e66f
Lee Driscoll <lee.driscoll@orbistech.co.uk>
2017-03-21 14:33:24 +0000

    Added basic node server

---
+++ source/_app/server.js
@@ -0,0 +1,22 @@
+import http from 'http'
+import {createLogger, stdSerializers} from 'bunyan'
+
+const log = createLogger({
+  name: 'Le Server',
+  serializers: {
+    err: stdSerializers.err
+  }
+})
+
+const PORT = process.env.NODE_PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.end("Hello World")
+})
+
+export default (cb) => {
+  server.listen(PORT, () => {
+    log.trace('It\'s alive!x!')
+    cb && cb()
+  })
+}
