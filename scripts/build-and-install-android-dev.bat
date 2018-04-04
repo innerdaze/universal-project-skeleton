073fd731c7d13cb3b1007d75dc09abf0f7b3f8a6
Lee Driscoll <lee.driscoll@orbistech.co.uk>
2018-04-04 14:22:58 +0100

    Updated android install batch script

--- scripts/build-and-install-android-dev.bat
+++ scripts/build-and-install-android-dev.bat
@@ -2,4 +2,4 @@
 call cross-env NODE_TARGET=device cross-env CORDOVA=true npm run build:cordova:dev
 call yarn cordova:link
 call cd cordova && cordova build android --device && ^
-adb uninstall uk.co.orbistech.mstock && adb install platforms/android/build/outputs/apk/android-debug.apk
+adb uninstall uk.co.orbistech.mstock && adb install platforms/android/build/outputs/apk/android-arm7-debug.apk
