0a132978a588ec47cdfd5d96d7e1773441e8a31d
Lee Driscoll <lee.driscoll@orbistech.co.uk>
2018-04-05 13:51:35 +0100

    Removed XWalk

--- cordova/platforms/android/assets/www/cordova_plugins.js
+++ cordova/platforms/android/assets/www/cordova_plugins.js
@@ -1,73 +1,57 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
+  module.exports = [
     {
-        "id": "cordova-plugin-device.device",
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "pluginId": "cordova-plugin-device",
-        "clobbers": [
-            "device"
-        ]
+      id: 'cordova-plugin-device.device',
+      file: 'plugins/cordova-plugin-device/www/device.js',
+      pluginId: 'cordova-plugin-device',
+      clobbers: ['device']
     },
     {
-        "id": "cordova-plugin-network-information.network",
-        "file": "plugins/cordova-plugin-network-information/www/network.js",
-        "pluginId": "cordova-plugin-network-information",
-        "clobbers": [
-            "navigator.connection",
-            "navigator.network.connection"
-        ]
+      id: 'cordova-plugin-network-information.network',
+      file: 'plugins/cordova-plugin-network-information/www/network.js',
+      pluginId: 'cordova-plugin-network-information',
+      clobbers: ['navigator.connection', 'navigator.network.connection']
     },
     {
-        "id": "cordova-plugin-network-information.Connection",
-        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
-        "pluginId": "cordova-plugin-network-information",
-        "clobbers": [
-            "Connection"
-        ]
+      id: 'cordova-plugin-network-information.Connection',
+      file: 'plugins/cordova-plugin-network-information/www/Connection.js',
+      pluginId: 'cordova-plugin-network-information',
+      clobbers: ['Connection']
     },
     {
-        "id": "cordova-plugin-sqlite-2.sqlitePlugin",
-        "file": "plugins/cordova-plugin-sqlite-2/dist/sqlite-plugin.js",
-        "pluginId": "cordova-plugin-sqlite-2",
-        "clobbers": [
-            "sqlitePlugin"
-        ]
+      id: 'cordova-plugin-sqlite-2.sqlitePlugin',
+      file: 'plugins/cordova-plugin-sqlite-2/dist/sqlite-plugin.js',
+      pluginId: 'cordova-plugin-sqlite-2',
+      clobbers: ['sqlitePlugin']
     },
     {
-        "id": "cordova-fabric-plugin.FabricPlugin",
-        "file": "plugins/cordova-fabric-plugin/www/FabricPlugin.js",
-        "pluginId": "cordova-fabric-plugin",
-        "clobbers": [
-            "window.fabric.core"
-        ]
+      id: 'cordova-fabric-plugin.FabricPlugin',
+      file: 'plugins/cordova-fabric-plugin/www/FabricPlugin.js',
+      pluginId: 'cordova-fabric-plugin',
+      clobbers: ['window.fabric.core']
     },
     {
-        "id": "cordova-fabric-plugin.FabricAnswersPlugin",
-        "file": "plugins/cordova-fabric-plugin/www/FabricPlugin.Answers.js",
-        "pluginId": "cordova-fabric-plugin",
-        "clobbers": [
-            "window.fabric.Answers"
-        ]
+      id: 'cordova-fabric-plugin.FabricAnswersPlugin',
+      file: 'plugins/cordova-fabric-plugin/www/FabricPlugin.Answers.js',
+      pluginId: 'cordova-fabric-plugin',
+      clobbers: ['window.fabric.Answers']
     },
     {
-        "id": "cordova-fabric-plugin.FabricCrashlyticsPlugin",
-        "file": "plugins/cordova-fabric-plugin/www/FabricPlugin.Crashlytics.js",
-        "pluginId": "cordova-fabric-plugin",
-        "clobbers": [
-            "window.fabric.Crashlytics"
-        ]
+      id: 'cordova-fabric-plugin.FabricCrashlyticsPlugin',
+      file: 'plugins/cordova-fabric-plugin/www/FabricPlugin.Crashlytics.js',
+      pluginId: 'cordova-fabric-plugin',
+      clobbers: ['window.fabric.Crashlytics']
     }
-];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-    "cordova-plugin-console": "1.0.6",
-    "cordova-plugin-device": "1.1.6",
-    "cordova-plugin-network-information": "1.3.2",
-    "cordova-plugin-sqlite-2": "1.0.4",
-    "cordova-plugin-whitelist": "1.3.2",
-    "cordova-fabric-plugin": "1.1.9",
-    "cordova-plugin-crosswalk-webview": "2.3.0"
-};
-// BOTTOM OF METADATA
-});
+  ]
+  module.exports.metadata =
+    // TOP OF METADATA
+    {
+      'cordova-plugin-console': '1.0.6',
+      'cordova-plugin-device': '1.1.6',
+      'cordova-plugin-network-information': '1.3.2',
+      'cordova-plugin-sqlite-2': '1.0.4',
+      'cordova-plugin-whitelist': '1.3.2',
+      'cordova-fabric-plugin': '1.1.9'
+    }
+  // BOTTOM OF METADATA
+})
